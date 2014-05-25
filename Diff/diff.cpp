24bf6938931d2eac804f4539510ef290bd80ed6f
ravikanth <ravikanthreddy89@gmail.com>
2014-05-25 03:16:54 -0400

    Checkpoint : Differentiation of given polynomianal expression.

---
+++ Diff/diff.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  int order;
+  cout<<"enter the order of the polynomial"<<endl;
+  cin>>order;
+  
+  vector<int> coeffs;
+  coeffs.resize(order);
+ 
+  for (int i = order-1; i >=0; i--)
+    {
+      cout<<"enter the coefficient of "<<order<<"power of x"<<endl;
+      cin>>coeffs[i];
+    }
+  
+  while(order>=1){
+    coeffs[order-1]=coeffs[order-1]*order;
+    order=order-1;
+  }
+
+  cout<<"The derivative of above expression is :"<<endl;
+  for (int i = coeffs.size()-1; i>=0; i--)
+    {
+      cout<<coeffs[i]<<" ";
+    }
+  cout<<endl;
+  return 0;
+}
